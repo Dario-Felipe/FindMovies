f0e916edd48d2635174828c8b93fccc50c0198d0
Dario-Felipe <dariofelipe91@gmail.com>
2020-09-02 17:14:06 -0300

    Create a React app and publish it to GitHub Pages.

---
+++ build/precache-manifest.a5c7a1e34c76a299124f56fa21e2acdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b589501b10e970d7f66b2f2c019589",
+    "url": "/FindMovies/index.html"
+  },
+  {
+    "revision": "9071cbe29489058786e0",
+    "url": "/FindMovies/static/css/main.6d4e473d.chunk.css"
+  },
+  {
+    "revision": "624343c4842bb80cbfea",
+    "url": "/FindMovies/static/js/2.6cb66e67.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FindMovies/static/js/2.6cb66e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9071cbe29489058786e0",
+    "url": "/FindMovies/static/js/main.79ddf96b.chunk.js"
+  },
+  {
+    "revision": "975fa608308b15649d1d",
+    "url": "/FindMovies/static/js/runtime-main.39ebc0f7.js"
+  }
+]);
